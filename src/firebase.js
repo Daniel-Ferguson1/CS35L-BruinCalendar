dcdd23abf4f8db3cda31e2d1b201e74f4ba6f4c6
lukeschoolmode <83619615+lukeschoolmode@users.noreply.github.com>
2021-05-10 22:56:36 -0700

    Wrapped so look like the doc.

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/app'
 import "firebase/auth"
-
-const app = firebase.initializeApp({
+// import 'firebase/firestore' // will do after test data 
+var firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -9,8 +9,10 @@ const app = firebase.initializeApp({
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-})
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  };
+const app = firebase.initializeApp(firebaseConfig)
+// wrapped it like Docs: https://firebase.google.com/docs/web/setup
 
 export const auth = app.auth()
 export default app 
