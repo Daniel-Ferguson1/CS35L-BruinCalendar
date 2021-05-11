3261db964991d6e2ac73f11f940f7cb855e3f524
lukeschoolmode <83619615+lukeschoolmode@users.noreply.github.com>
2021-05-10 18:00:15 -0700

    Set uid one of what's in our current prj.

--- test/testData.js
+++ test/testData.js
@@ -6,7 +6,7 @@
 const events = [{
     dateTime: '2022-04-10 13:00',
     eventid: '12312',
-    userId: 'AJAJ0DmiHogY1iNElgtAvt7GBsE3',
+    userId: 'uwjGvRH7OPUrKb0uXgY53xZdGUK2',
     eventName: "Quinceañera",
     urgency: 1, // like deadline or soemthing.
     schoolness: 1, // what was this for?
@@ -14,7 +14,7 @@ const events = [{
 }, {
     dateTime: '2031-07-16 04:21',
     eventid: '72773',
-    userId: 'AJAJ0DmiHogY1iNElgtAvt7GBsE3',
+    userId: 'uwjGvRH7OPUrKb0uXgY53xZdGUK2',
     eventName: "Some Event",
     urgency: 0,
     schoolness: 1,
@@ -22,7 +22,7 @@ const events = [{
 }, {
     dateTime: '2011-09-15 07:30', //past
     eventid: '15531',
-    userId: 'AJAJ0DmiHogY1iNElgtAvt7GBsE3',
+    userId: 'uwjGvRH7OPUrKb0uXgY53xZdGUK2',
     eventName: "Such and Such Event",
     urgency: 1,
     schoolness: 1,
@@ -30,7 +30,7 @@ const events = [{
 }, {
     dateTime: '2011-08-26 06:30', 
     eventid: '12345',
-    userId: 'AJAJ0DmiHogY1iNElgtAvt7GBsE3',
+    userId: 'uwjGvRH7OPUrKb0uXgY53xZdGUK2',
     eventName: "Such and Such Event",
     urgency: 1,
     schoolness: 1,
@@ -38,7 +38,7 @@ const events = [{
 }, {
     dateTime: '2024-05-18 04:13', 
     eventid: '48671',
-    userId: 'AJAJ0DmiHogY1iNElgtAvt7GBsE3',
+    userId: 'uwjGvRH7OPUrKb0uXgY53xZdGUK2',
     eventName: "Such and Such Event",
     urgency: 1,
     schoolness: 1,
@@ -46,7 +46,7 @@ const events = [{
 }, {
     dateTime: '2022-08-02 11:10',
     eventid: '40293',
-    userId: 'AJAJ0DmiHogY1iNElgtAvt7GBsE3',
+    userId: 'uwjGvRH7OPUrKb0uXgY53xZdGUK2',
     eventName: "Such and Such Event",
     urgency: 1,
     schoolness: 1,
@@ -54,7 +54,7 @@ const events = [{
 }, {
     dateTime: '2026-03-02 15:20', 
     eventid: '96823',
-    userId: 'AJAJ0DmiHogY1iNElgtAvt7GBsE3',
+    userId: 'uwjGvRH7OPUrKb0uXgY53xZdGUK2',
     eventName: "Such and Such Event",
     urgency: 1,
     schoolness: 1,
@@ -62,7 +62,7 @@ const events = [{
 }, {
     dateTime: '2022-08-02 11:52',
     eventid: '48821',
-    userId: 'AJAJ0DmiHogY1iNElgtAvt7GBsE3',
+    userId: 'uwjGvRH7OPUrKb0uXgY53xZdGUK2',
     eventName: "Such and Such Event",
     urgency: 1,
     schoolness: 1,
@@ -70,7 +70,7 @@ const events = [{
 }, {
     dateTime: '2022-11-02 08:34', 
     eventid: '32911',
-    userId: 'AJAJ0DmiHogY1iNElgtAvt7GBsE3',
+    userId: 'uwjGvRH7OPUrKb0uXgY53xZdGUK2',
     eventName: "Such and Such Event",
     urgency: 1,
     schoolness: 1,
@@ -78,7 +78,7 @@ const events = [{
 }, {
     dateTime: '2024-05-02 04:10', 
     eventid: '43814',
-    userId: 'AJAJ0DmiHogY1iNElgtAvt7GBsE3',
+    userId: 'uwjGvRH7OPUrKb0uXgY53xZdGUK2',
     eventName: "Such and Such Event",
     urgency: 1,
     schoolness: 1,
