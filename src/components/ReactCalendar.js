f53850b6d883cc58ebd56fb231f0fb37e82423df
Young Sung Lyun <72766090+aspboy217@users.noreply.github.com>
2021-05-24 17:56:18 -0700

    Update ReactCalendar.js

--- src/components/ReactCalendar.js
+++ src/components/ReactCalendar.js
@@ -2,15 +2,36 @@ import React, { useState } from "react";
 import Calendar from "react-calendar";
 import './ReactCalendar.css'
 
+import Event from './Event'
+
+function clickMe(){
+  alert('You clicked me!');
+}
+
 const ReactCalendar = () => {
   const [date, onChange] = useState(new Date());
-  // const onChange = date => { setDate(date); };
+
+  function whenClickDay(date) {
+    alert(date);
+    // Send the date information to the DB and 
+    // receive event information to Event.js
+    return(
+      <Event>
+        Fancy Modal
+      </Event>
+    )
+  }
 
   return (
     <div>
-      <Calendar onChange={onChange} value={date} calendarType={"US"} />
+      <Calendar 
+        onChange={onChange} 
+        value={date} 
+        calendarType={"US"} 
+        onClickDay={whenClickDay}
+      />
     </div>
-  );
-};
+  )
+}
 
 export default ReactCalendar;
